96bdef97f8af50d4c7b8d0637bcfde416969d695
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-01-18 00:37:59 +0100

    attack pending

--- test/character.test.js
+++ test/character.test.js
@@ -18,4 +18,34 @@ describe('Characters', () => {
         }
         )
     })
+
+    /* 2. Characters can Deal Damage to Characters.
+    - Damage is subtracted from Health
+    - When damage received exceeds current Health,
+    Health becomes 0 and the character dies */
+
+    describe('Character deals damage to another', () => {
+        const attackerCharacter = {
+            alive: true,
+            agility: 10,
+            dexterity: 5,
+            intelligence: 5,
+            health: 10,
+            stamina: 10,
+        }
+        const targetCharacter = {
+            alive: true,
+            agility: 10,
+            dexterity: 5,
+            intelligence: 5,
+            health: 10,
+            stamina: 10,
+        }
+
+        it('attack', () => {
+            const result = attackerCharacter.attack(targetCharacter)
+            expect(result).toHaveProperty('health', )
+        }
+        )
+    })
 })
