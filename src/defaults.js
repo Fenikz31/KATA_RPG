06eeee2fb948c6eba33f6f78aac76a5b76ffb8fd
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-01-17 17:32:29 +0100

    RACES updated

--- src/defaults.js
+++ src/defaults.js
@@ -16,20 +16,35 @@ export const CHARACTER = {
 
 export const RACES = {
   ELF: {
+    agilty: 5,
     damage: 1,
+    dexterity: 5,
     heal: 2,
-    health: 15
+    health: 15,
+    intelligence: 5,
+    mana: 5,
+    stamina: 5
   },
 
   HUMAN: {
+    agilty: 5,
     damage: 1,
+    dexterity: 5,
     heal: 1,
-    health: 20
+    health: 20,
+    intelligence: 5,
+    mana: 5,
+    stamina: 5
   },
 
   ORC: {
+    agilty: 5,
     damage: 2,
+    dexterity: 5,
     heal: -1,
-    health: 10
+    health: 10,
+    intelligence: 5,
+    mana: 5,
+    stamina: 5
   }
 }
