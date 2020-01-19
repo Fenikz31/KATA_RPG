32ab164b7a71b75406ddfee06ef1f38366cc9fd7
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-01-19 22:39:35 +0100

    actions updated

--- src/actions.js
+++ src/actions.js
@@ -1,35 +1,35 @@
 import { CHARACTERS } from './descriptors'
 
-function attack(target) {
+export function attack(target) {
     return {
         type: CHARACTERS.ATTACK,
         payload: target
     }
 }
 
-function create() {
+export function create() {
     { type: CHARACTERS.CREATE }
 }
 
-function gainXp(xp) {
+export function gainXp(xp) {
     return {
         type: CHARACTERS.GAIN_XP,
         payload: xp
     }
 }
 
-function heal(target) {
+export function heal(target) {
     return {
         type: CHARACTERS.HEAL,
         payload: target
     }
 }
 
-function levelUp() {
+export function levelUp() {
     return { type: CHARACTERS.LEVEL_UP }
 }
 
-function move(x, y) {
+export function move(x, y) {
     return {
         type: CHARACTERS.MOVE,
         payload: { x, y }
