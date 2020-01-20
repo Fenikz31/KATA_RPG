bd8d43189d4a6f9fd490566ce8ab1b84dac1d690
Patrick Kaninda <patrick.kaninda@interactiv-group.com>
2020-01-20 09:47:38 +0100

    index.js done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-import { CHARACTERS } from './src/descriptors'
+import { CHARACTERS } from './descriptors'
+import Store from './store';
+
+Store.dispatch({type: CHARACTERS.CREATE, name: 'pkan' })
+Store.dispatch({type: CHARACTERS.CREATE, name: 'fera' })
